e285486c95d32e8edff4103b7afcdee0f35699c0
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-07-16 16:13:45 -0300

    add task entity

--- src/main/java/com/chaosprojectbr/todolistservice/domain/entities/Task.java
+++ src/main/java/com/chaosprojectbr/todolistservice/domain/entities/Task.java
@@ -2,8 +2,16 @@ package com.chaosprojectbr.todolistservice.domain.entities;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("task_document")
 public class Task {
+
+    public Task(Builder builder) {
+        this.title = builder.title;
+        this.subtitle = builder.subtitle;
+        this.description = builder.description;
+    }
 
     @Id
     private ObjectId _id;
@@ -11,11 +19,30 @@ public class Task {
     private String title;
     private String subtitle;
     private String description;
-    private User user;
 
-    private static class User {
-        private String user;
-        private String email;
+    static class Builder {
+
+        private String title;
+        private String subtitle;
         private String description;
+
+        public Task build() {
+            return new Task(this);
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setSubtitle(String subtitle) {
+            this.subtitle = subtitle;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
     }
 }
